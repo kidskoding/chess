f8da52ee26493b0e070ec145e62a502a62bc18c7
kidskoding <kidskodingclub@gmail.com>
2024-06-26 22:58:46 -0500

    added Square class

--- src/classes/square.py
+++ src/classes/square.py
@@ -1,43 +1,38 @@
-# Example file showing a circle moving on screen
 import pygame
 
-# pygame setup
-pygame.init()
-screen = pygame.display.set_mode((1280, 720))
-clock = pygame.time.Clock()
-running = True
-dt = 0
-
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
-
-    pygame.draw.circle(screen, "red", player_pos, 40)
-
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
-
-    # flip() the display to put your work on screen
-    pygame.display.flip()
-
-    # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
-    dt = clock.tick(60) / 1000
-
-pygame.quit()
+class Square:
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.abs_x = x * width
+        self.abs_y = y * height
+        self.abs_pos = (self.abs_x, self.abs_y)
+        self.pos = (x, y)
+        self.color = 'light' if (x + y) % 2 == 0 else 'dark'
+        self.draw_color = (220, 208, 194) if self.color == 'light' else (53, 53, 53)
+        self.highlight_color = (100, 249, 83) if self.color == 'light' else (0, 228, 10)
+        self.occupying_piece = None
+        self.coord = self.get_coord()
+        self.highlight = False
+        self.rect = pygame.Rect(self.abs_x, self.abs_y, self.width, self.height)
+        
+    # get the formal notation of the tile    
+    def get_coord(self):
+        files = 'abcdefgh' #in chess, files = columns
+        ranks = '12345678' #in chess, ranks = rows
+        return files[self.x] + ranks[self.y]
+    
+    #draw the square onto the board
+    def draw(self, display):
+        if self.highlight:
+            pygame.draw.rect(display, self.highlight_color, self.rect)
+        else:
+            pygame.draw.rect(display, self.draw_color, self.rect)
+        
+        if self.occupying_piece != None:
+            centering_rect = self.occupying_piece.img.get_rect()
+            centering_rect.center = self.rect.center
+            display.bilt(self.occupying_piece.img, centering_rect.topleft)
+        
