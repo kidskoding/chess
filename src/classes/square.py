a598e7a9ed2979513ae1d6bfef7a5eb92fe21ef4
kidskoding <kidskodingclub@gmail.com>
2024-06-26 23:43:40 -0500

    modified code for Square class

--- src/classes/square.py
+++ src/classes/square.py
@@ -1,38 +1,50 @@
 import pygame
 
 class Square:
-    def __init__(self, x, y, width, height):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.abs_x = x * width
-        self.abs_y = y * height
-        self.abs_pos = (self.abs_x, self.abs_y)
+    WIDTH = 80
+    HEIGHT = 80
+    
+    ''' 
+        Squares have 
+        an x and y index
+        coordinate ( (x, y) )
+        occupying piece
+        highlight state (True or False)
+        a draw and highlight color 
+        a pygame rectangle that defines the square
+    '''
+    def __init__(self, x, y):
+        self.x = x;
+        self.y = y;
         self.pos = (x, y)
+        self.abs_x = x * Square.WIDTH
+        self.abs_y = y * Square.HEIGHT
+        self.abs_pos = (self.abs_x, self.abs_y)
+        self.coord = self.get_coord()
+        self.occupying_piece = None
+        self.isHighlighted = False
         self.color = 'light' if (x + y) % 2 == 0 else 'dark'
         self.draw_color = (220, 208, 194) if self.color == 'light' else (53, 53, 53)
         self.highlight_color = (100, 249, 83) if self.color == 'light' else (0, 228, 10)
-        self.occupying_piece = None
-        self.coord = self.get_coord()
-        self.highlight = False
-        self.rect = pygame.Rect(self.abs_x, self.abs_y, self.width, self.height)
-        
-    # get the formal notation of the tile    
-    def get_coord(self):
-        files = 'abcdefgh' #in chess, files = columns
-        ranks = '12345678' #in chess, ranks = rows
-        return files[self.x] + ranks[self.y]
+        self.rect = pygame.rect(
+            self.abs_x,
+            self.abs_y,
+            Square.WIDTH,
+            Square.HEIGHT
+        )
     
-    #draw the square onto the board
+    # Get coordinate of Square in Chess terms (Coordinate (0, 0) would be 'a1')
+    def get_coord(self):
+        files = 'abcdefgh'
+        return files[self.x] + str(self.y + 1)
+    
     def draw(self, display):
-        if self.highlight:
+        # Draw the square
+        if self.isHighlighted:
             pygame.draw.rect(display, self.highlight_color, self.rect)
         else:
             pygame.draw.rect(display, self.draw_color, self.rect)
-        
-        if self.occupying_piece != None:
-            centering_rect = self.occupying_piece.img.get_rect()
-            centering_rect.center = self.rect.center
-            display.bilt(self.occupying_piece.img, centering_rect.topleft)
-        
+            
+        # Draw the piece
+        if self.occupying_piece is not None:
+            display.bilt(self.occupying_piece.img, self.abs_pos)
--- src/main.py
+++ src/main.py
@@ -1,1 +1,16 @@
 import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((600, 600))
+pygame.display.set_caption('Chess')
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    screen.fill((255, 255, 255))
+    pygame.display.flip()
+            
+pygame.quit()
