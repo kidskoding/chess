bc03f2a9422f7332f44245d40e35e369df510a04
Anirudh Konidala <68204671+kidskoding@users.noreply.github.com>
2024-07-04 01:26:44 -0500

    added king movement

--- src/classes/pieces/king.py
+++ src/classes/pieces/king.py
@@ -10,3 +10,57 @@ class King(Piece):
         else:
             self.img = pygame.image.load('src/imgs/b_king.png')
         self.img = pygame.transform.scale(self.img, (board.tile_width / 2, board.tile_height / 2))
+        
+    def get_available_moves(self, board):
+        available_moves = []
+        current_x, current_y = self.x, self.y
+        
+        # Up
+        if(current_y - 1 > -1): 
+            up = board.get_square((current_x, current_y - 1))
+            if(self.can_move(up) or self.can_capture(up)):
+                available_moves.append(up)
+                
+        # Down
+        if(current_y + 1 < 8):
+            down = board.get_square((current_x, current_y + 1))
+            if(self.can_move(down) or self.can_move(down)):
+                available_moves.append(down)
+                
+        # Left
+        if(current_x - 1 > -1):
+            left = board.get_square((current_x - 1, current_y))
+            if(self.can_move(left) or self.can_capture(left)):
+                available_moves.append(left)
+                
+        # Right
+        if(current_x + 1 < 8):
+            right = board.get_square((current_x + 1, current_y))
+            if(self.can_move(right) or self.can_capture(left)):
+                available_moves.append(right)
+                
+        # Top Left Diagonal
+        if(current_x - 1 > -1 and current_y - 1 > -1):
+            top_left = board.get_square((current_x - 1, current_y - 1))
+            if(self.can_move(top_left) or self.can_capture(top_left)):
+                available_moves.append(top_left)
+                
+        # Top Right Diagonal
+        if(current_x + 1 < 8 and current_y - 1 > -1):
+            top_right = board.get_square((current_x + 1, current_y - 1))
+            if(self.can_move(top_right) or self.can_capture(top_right)):
+                available_moves.append(top_right)
+        
+        # Bottom Left Diagonal
+        if(current_x - 1 > -1 and current_y + 1 < 8):
+            bottom_left = board.get_square((current_x - 1, current_y + 1))
+            if(self.can_move(bottom_left) or self.can_capture(bottom_left)):
+                available_moves.append(bottom_left) 
+                
+        # Bottom Right Diagonal
+        if(current_x + 1 < 8 and current_y + 1 < 8):
+            bottom_right = board.get_square((current_x + 1, current_y + 1))
+            if(self.can_move(bottom_right) or self.can_capture(bottom_right)):
+                available_moves.append(bottom_right)
+                
+        return available_moves
